1816cb5932084f2617097c15a205a31d5e5af4f1
Aspoda <ssmoreda@gmail.com>
2024-09-10 18:57:28 -0700

    Se agregaron las funciones de is_valid y Sintax_checker

--- romanos/romanos.py
+++ romanos/romanos.py
@@ -8,8 +8,6 @@ dict_romanos = {
     'M' : 1000
 }
 
-input = 'xlivn'
-input = input.upper()
 
 def is_roman(input):
     for letra in input:
@@ -41,9 +39,40 @@ def evaluar_valores(array):
             result.append(array[i])
     print(sum(result))
 
+def is_valid(array):
+    result = []
+    bandera = 0
+    for i in range(len(array)-1):
+        if input[i] == 'V' or input[i] == 'L' or input[i] == 'D':
+            if input[i] == input[i+1]:
+                print(f'No pueden ir dos {input[i]} seguidas')
+        if input[i] == 'I' or input[i] == 'X' or input[i] == 'C' or input[i] == 'M':
+            if input[i] == input[i+1]:
+                bandera +=1
+    if bandera > 2:
+            print("no puede haber mas de 3 letras iguales")    
+
+def sintax_checker(input):
+    for i in range(len(input)-1):
+        if input[i] == 'I' or input[i] == 'L' :
+            if input[i+1] != 'I' and input[i+1] != 'V' and input[i+1] != 'X':
+                print(f'error de sintaxis despues de {input[i]} solo puede ir V o X ') 
+        if input[i] == 'V':
+            if input[i+1] != 'I':
+                print(f'error de sintaxis despues de {input[i]} solo puede ir I')
+        if input[i] == 'X':
+            if input[i+1] == 'M':
+                print(f'error de sintaxis despues de {input[i]} solo puede ir I,V,X,L o C ')  
+            
 ##m main
+input = 'vLCn'
+input = input.upper()
+
 
 is_roman(input)
+is_valid(input)
+sintax_checker(input)
+
 array_romanos = separate_roman(input)
 print(array_romanos)
 valores = numerar_romanos(array_romanos)
